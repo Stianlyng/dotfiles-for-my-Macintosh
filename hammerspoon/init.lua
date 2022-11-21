25fc757b2d06e073817733d139d3ef6d1edf753d
Stian Lyng <71536569+Stianlyng@users.noreply.github.com>
2022-11-21 10:09:52 +0100

    updated

---
+++ hammerspoon/init.lua
@@ -0,0 +1,15 @@
+----------------------------------------------------------------------
+--                            Constants                             --
+----------------------------------------------------------------------
+
+
+--function log2obs()
+--    hs.execute('python3 /Users/stig/.config/obsidian-logseq/logseqObsidianSync.py')
+--end
+---
+--myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/Documents/obsidian/logseq/pages", log2obs):start()
+
+
+
+
+
